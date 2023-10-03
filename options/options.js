d0dee03c690f8b88035bedd34800f893ff5efc5a
Crowdbotics <team@crowdbotics.com>
2023-10-03 23:22:58 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "purple-wind-43745",
+  url: "https://purple-wind-43745.botics.co",
+  api: "https://purple-wind-43745.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
